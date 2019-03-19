550cc856f9aa8bbc27dc29b12c738bb21c73fef3
Ricardo Ruy <ricardo.rruy@hotmail.com>
2019-03-19 04:14:21 -0300

    Update payment_product_physical.rb

--- ddd-refactoriing-ruby/domain/payment_product_physical.rb
+++ ddd-refactoriing-ruby/domain/payment_product_physical.rb
@@ -1,15 +1,14 @@
 class Domain::PaymentProductPhysical < Domain::PaymentProduct
         
-    def type
-      :"physical"
-    end
+  def type
+    :"physical"
+  end
         
-    #Info: Se o pagamento for para um item físico, você deverá gerar um shipping label para o mesmo ser colocado na caixa do envio;
-    def execute(order_item)
-        shipping_label = ShippimentLabel.new(order_item)
-        shipping_label.save
-        
-        Cargo.new(order_item, shipping_label).save
-    end
+  #Info: Se o pagamento for para um item físico, você deverá gerar um shipping label para o mesmo ser colocado na caixa do envio;
+  def execute(order_item)
+    shipping_label = ShippimentLabel.new(order_item)
+    shipping_label.save
+    Cargo.new(order_item, shipping_label).save
+  end
 end
 

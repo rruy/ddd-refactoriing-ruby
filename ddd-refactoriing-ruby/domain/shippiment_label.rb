42a1083a4b5ba5f77f5e355d63d0e751b33e52c8
Ricardo Ruy <ricardo.rruy@hotmail.com>
2019-03-19 04:15:09 -0300

    Update shippiment_label.rb

--- ddd-refactoriing-ruby/domain/shippiment_label.rb
+++ ddd-refactoriing-ruby/domain/shippiment_label.rb
@@ -1,18 +1,16 @@
 class ShippimentLabel
+  attr_accessor :label
+  attr_accessor :order_item
+  attr_accessor :address_shippiment
     
-    attr_accessor :label
-    attr_accessor :order_item
-    attr_accessor :address_shippiment
+  def initialize(label, order_item, address_shippiment)
+    @label = "label- #{order_item.id}"
+    @order_item = order_item
+    @address_shippiment = address_shippiment
+    @notification = "Item Isento de imposto"
+  end
     
-    def initialize(label, order_item, address_shippiment)
-        @label = "label- #{order_item.id}"
-        @order_item = order_item
-        @address_shippiment = address_shippiment
-        @notification = "Item Isento de imposto"
-    end
-    
-    def save
-      raise NotImplementedError, '' 
-    end
-    
+  def save
+    raise NotImplementedError, '' 
+  end
 end

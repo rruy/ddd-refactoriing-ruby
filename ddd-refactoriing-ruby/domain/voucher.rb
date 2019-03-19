82c20caba63c2553c0be43447b69217ca1a2c0cf
Ricardo Ruy <ricardo.rruy@hotmail.com>
2019-03-19 04:15:36 -0300

    Update voucher.rb

--- ddd-refactoriing-ruby/domain/voucher.rb
+++ ddd-refactoriing-ruby/domain/voucher.rb
@@ -1,14 +1,13 @@
 class Domain::Voucher
+  attr_accessor :customer
+  attr_accessor :credit
     
-    attr_accessor :customer
-    attr_accessor :credit
+  def initialize(customer, credit)
+    @customer = customer
+    @credit = credit
+  end
     
-    def initialize(customer, credit)
-        @customer = customer
-        @credit = credit
-    end
-    
-    def save
-       raise NotImplementedError, '' 
-    end
+  def save
+    raise NotImplementedError, '' 
+  end
 end

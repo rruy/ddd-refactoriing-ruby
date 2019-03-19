6b41ff5452f600acc553319326ff88a20f445e2a
Ricardo Ruy <ricardo.rruy@hotmail.com>
2019-03-19 04:13:18 -0300

    Update payment_product_digital.rb

--- ddd-refactoriing-ruby/domain/payment_product_digital.rb
+++ ddd-refactoriing-ruby/domain/payment_product_digital.rb
@@ -1,21 +1,18 @@
 class Domain::PaymentProductDigital < Domain::PaymentProduct
+  attr_accessor :order_item
         
-    attr_accessor :order_item
-        
-    def type
-        :"digital"
-    end
+  def type
+    :"digital"
+  end
        
-    #Info:: Caso o pagamento seja de alguma mídia digital (música, vídeo), além de enviar a descrição da compra por e-mail ao comprador, 
-    # conceder um voucher de desconto de R$ 10 ao comprador associado ao pagamento. 
-    def execute(order_item)
-       customer = order_item.order.customer
-       message = "O pagamento do seu pedido #{order_item.order.id} foi aprovado!"
-       
-       Infrastructure::MailerService.send(customer.email, "", "Pagamento aprovado", message)
-       
-       Domain::Voucher.new(customer, 10).save
-    end
+  #Info:: Caso o pagamento seja de alguma mídia digital (música, vídeo), além de enviar a descrição da compra por e-mail ao comprador, 
+  # conceder um voucher de desconto de R$ 10 ao comprador associado ao pagamento. 
+  def execute(order_item)
+    customer = order_item.order.customer
+    message = "O pagamento do seu pedido #{order_item.order.id} foi aprovado!"
+    Infrastructure::MailerService.send(customer.email, "", "Pagamento aprovado", message)
+    Domain::Voucher.new(customer, 10).save
+  end
 end
 
 
